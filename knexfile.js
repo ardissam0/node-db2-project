be61953073ad6d8cafdaee6010c60460d69714b5
Sam Ardis <ardis.productions@gmail.com>
2020-04-15 14:42:46 -0400

    Added main files starting code, working on migration

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    development: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/cars.db3'
+      }, 
+      useNullAsDefault: true,
+    },
+  
+  };
