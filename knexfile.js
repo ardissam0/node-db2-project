d53488b35c30bc99dd940e4001211182b45341ad
Sam Ardis <ardis.productions@gmail.com>
2020-04-15 15:53:11 -0400

    Added CRUD operations, working on getting data to display, not sure why just [] is coming back

--- knexfile.js
+++ knexfile.js
@@ -5,16 +5,47 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/cars.db3'
+      filename: './data/cars.db3',
     },
-    useNullAsDefault: true
-  },
+    useNullAsDefault: true,
 
     migrations: {
       directory:'./data/migrations'
-    },
 
+    },
     seeds:{
-      directory:'.data/seeds'
-    }
+      directory:'.data/seeds',
+    },
+  },
+  staging: {
+    client: "postgresql",
+    connection: {
+        database: "my_db",
+        user: "username",
+        password: "password",
+    },
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    migrations: {
+        tableName: "knex_migrations",
+    },
+},
+
+production: {
+    client: "postgresql",
+    connection: {
+        database: "my_db",
+        user: "username",
+        password: "password",
+    },
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    migrations: {
+        tableName: "knex_migrations",
+    },
+  },
 };
